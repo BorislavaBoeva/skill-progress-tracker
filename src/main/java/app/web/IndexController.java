f47ab1dd007010c63e5fe62b3f1eac617af68e2f
BorislavaBoeva <rivaroha@gmail.com>
2026-06-04 21:45:36 +0300

    fix: remove unused CategoryService refactor: add login method to IndexController

--- src/main/java/app/web/IndexController.java
+++ src/main/java/app/web/IndexController.java
@@ -1,15 +1,22 @@
 package app.web;
 
+import app.model.entity.dto.user.UserLoginRequestDto;
 import app.model.entity.dto.user.UserRegisterRequestDto;
+import app.service.user.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 
-    //празна форма-> object
-    UserRegisterRequestDto userRegisterRequest = UserRegisterRequestDto.builder().build();
+
+    private final UserService userService;
+
+    public IndexController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/")
     public String index() {
@@ -24,15 +31,23 @@ public class IndexController {
 
     @GetMapping("/login")
     public ModelAndView getLogin() {
+        //празна форма-> object
+        UserRegisterRequestDto userRegisterRequest = UserRegisterRequestDto.builder().build();
         ModelAndView modelAndView = new ModelAndView("login");
         modelAndView.addObject("userRegisterRequest", userRegisterRequest);
 
         return modelAndView;
     }
 
+    @GetMapping("/login")
+    public ModelAndView getLogin(@RequestParam UserLoginRequestDto userLoginRequest) {
+      userService.login(userLoginRequest);
+        return new ModelAndView("redirect:/home");
+    }
+
     @GetMapping("/home")
-    public String home() {
-        return "home";
+    public ModelAndView getHomePage() {
+        return new ModelAndView("home");
     }
 
     @GetMapping("/skill")
